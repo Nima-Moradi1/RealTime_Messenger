bcb9286d53be854467a52eb2f31fa7846f9c7183
nima moradi <mnima8100@gmail.com>
2025-03-09 21:47:56 +0330

    add AuthSocialButton component for social authentication integration

---
+++ src/app/(site)/components/AuthSocialButton.tsx
@@ -0,0 +1,23 @@
+
+import { IconType } from 'react-icons'
+import React from 'react'
+
+interface AuthSocialButtonProps {
+    icon : IconType ,
+    onClick : () => void
+}
+
+const AuthSocialButton : React.FC<AuthSocialButtonProps> = ({icon:Icon , onClick}) => {
+  return (
+   <button
+   type='button'
+   onClick={onClick}
+   className='inline-flex w-full justify-center rounded-md bg-background px-4 py-2 text-foreground/60 shadow-sm ring-1 hover:bg-gray-50
+   focus:outline-offset-0'
+   >
+    <Icon />
+   </button>
+  )
+}
+
+export default AuthSocialButton
