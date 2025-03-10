b9cfc65a6bbc7f2fb2c1ec4cb07437a16de9fb51
nima moradi <mnima8100@gmail.com>
2025-03-10 18:20:41 +0330

    add: create UsersLayout and UsersPage components for user management interface

---
+++ src/app/users/layout.tsx
@@ -0,0 +1,13 @@
+import Sidebar from "@/components/sidebar/Sidebar"
+
+const UsersLayout = async ({children} : {children:React.ReactNode}) => {
+  return (
+    <Sidebar>
+    <div className='h-full'>
+        {children}
+    </div>
+    </Sidebar>
+  )
+}
+
+export default UsersLayout
