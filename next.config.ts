9fe34b7b9c6a1c0532fc0117421a046edbae8037
nima moradi <mnima8100@gmail.com>
2025-03-10 18:37:44 +0330

    removed next-superjson-plugin because of conflict with next-15

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental : {
-    swcPlugins : [
-      ["next-superjson-plugin", {}]
-    ]
-  }
 };
 
 export default nextConfig;
