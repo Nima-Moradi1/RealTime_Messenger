8c368d53749ed62b0b5ad31cbeb0114222324289
nima moradi <mnima8100@gmail.com>
2025-03-10 16:22:52 +0330

    fix: update AuthSocialButton styles to include cursor pointer on focus

--- src/app/(site)/components/AuthSocialButton.tsx
+++ src/app/(site)/components/AuthSocialButton.tsx
@@ -13,7 +13,7 @@ const AuthSocialButton : React.FC<AuthSocialButtonProps> = ({icon:Icon , onClick
    type='button'
    onClick={onClick}
    className='inline-flex w-full justify-center rounded-md bg-background px-4 py-2 text-foreground/60 shadow-sm ring-1 hover:bg-gray-50
-   focus:outline-offset-0'
+   focus:outline-offset-0 cursor-pointer'
    >
     <Icon />
    </button>
