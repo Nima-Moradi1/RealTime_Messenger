21a25e5469c2332aff5e30f57e580a07f8fa43f9
nima moradi <mnima8100@gmail.com>
2025-03-10 18:21:17 +0330

    fix: update background color in EmptyState to dynamic value

--- src/components/EmptyState.tsx
+++ src/components/EmptyState.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const EmptyState = () => {
   return (
     <div
-    className='px-4 py-10 sm:px-6 lg:px-8 h-full flex justify-center bg-gray-100'
+    className='px-4 py-10 sm:px-6 lg:px-8 h-full flex justify-center bg-gray-2'
     >
         <div
         className='text-center items-center flex justify-center flex-col'>
