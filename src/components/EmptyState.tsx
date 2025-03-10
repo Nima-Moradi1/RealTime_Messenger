e348f6830920bda1aed8166f19c889788792b60a
nima moradi <mnima8100@gmail.com>
2025-03-10 16:27:58 +0330

    add: create EmptyState component for chat interface

---
+++ src/components/EmptyState.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const EmptyState = () => {
+  return (
+    <div
+    className='px-4 py-10 sm:px-6 lg:px-8 h-full flex justify-center bg-gray-100'
+    >
+        <div
+        className='text-center items-center flex justify-center flex-col'>
+            <h3
+            className='mt-2 text-2xl font-semibold text-foreground'
+            >Select a chat or start a conversation</h3>
+        </div>
+    </div>
+  )
+}
+
+export default EmptyState
