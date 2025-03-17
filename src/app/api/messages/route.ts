27f50b66d4223bff0616fa23385911f3764d58a5
nima moradi <mnima8100@gmail.com>
2025-03-17 23:35:06 +0330

    fix: update Pusher event name from 'message:new' to 'messages:new' for consistency

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -63,7 +63,7 @@ export async function POST(request:Request) {
                 }
             }
         })
-        await pusherServer.trigger(conversationId , 'message:new', newMessage)
+        await pusherServer.trigger(conversationId , 'messages:new', newMessage)
         const lastMessage = updatedConversation.messages[updatedConversation.messages.length - 1];
         updatedConversation.users.map((user)=> (
             pusherServer.trigger(user.email! , 'conversation:update' , {
