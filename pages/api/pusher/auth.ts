3f311f0f30bee93cc4476fcda5f9ee6d8ac738a4
nima moradi <mnima8100@gmail.com>
2025-03-19 15:55:35 +0330

    fix nextjs error : moved authOptions to a seperate file

--- pages/api/pusher/auth.ts
+++ pages/api/pusher/auth.ts
@@ -3,7 +3,7 @@
 
 import { NextApiRequest , NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/libs/authOptions";
 import { pusherServer } from "@/libs/pusher";
 
 
