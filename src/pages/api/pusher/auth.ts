d5feb57daf98c01f8eee89965ee0594ca07f6b92
nima moradi <mnima8100@gmail.com>
2025-03-19 13:33:10 +0330

    add: implement Pusher authentication handler for user active status

---
+++ src/pages/api/pusher/auth.ts
@@ -0,0 +1,25 @@
+//Since Pusher does not support some features for app router, we created a pages directory
+//To write the logic for Active status of users
+
+import { NextApiRequest , NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { pusherServer } from "@/libs/pusher";
+
+
+export async function handler(request:NextApiRequest, response : NextApiResponse){
+    const session = await getServerSession(request , response , authOptions) ;
+    if(!session?.user?.email) {
+        return response.status(401);
+    }
+
+    const socketId = request.body.socket_id ;
+    const channel = request.body.channel_name ;
+    const data = {
+        user_id : session.user.email
+    }
+
+    const authResponse = pusherServer.authorizeChannel(socketId , channel,data);
+
+    return response.send(authResponse)
+}
